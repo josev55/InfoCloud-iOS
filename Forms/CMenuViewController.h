a06b91c1325bc58c4b92604dc769774b007f202e
Jose Vildosola <josev55@gmail.com>
2013-08-12 16:39:24 -0400

    Test 2

---
+++ Forms/CMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMenuViewController.h
+//  Forms
+//
+//  Created by Jose Vildosola on 12-08-13.
+//  Copyright (c) 2013 Jose Vildosola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMenuViewController : UIViewController
+
+@end
---
+++ Forms/CMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  CMenuViewController.m
+//  Forms
+//
+//  Created by Jose Vildosola on 12-08-13.
+//  Copyright (c) 2013 Jose Vildosola. All rights reserved.
+//
+
+#import "CMenuViewController.h"
+
+@interface CMenuViewController ()
+
+@end
+
+@implementation CMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
