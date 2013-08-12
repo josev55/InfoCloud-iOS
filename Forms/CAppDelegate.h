25d8a118b9b47bd5b34e38ec354c0dfa5ec392eb
Jose Vildosola <josev55@gmail.com>
2013-08-12 16:25:57 -0400

    Initial Commit

---
+++ Forms/CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAppDelegate.h
+//  Forms
+//
+//  Created by Jose Vildosola on 12-08-13.
+//  Copyright (c) 2013 Jose Vildosola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Forms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Forms
+//
+//  Created by Jose Vildosola on 12-08-13.
+//  Copyright (c) 2013 Jose Vildosola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAppDelegate class]));
+    }
+}
